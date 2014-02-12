57b3c6e9795ea1bd111c7bd42820ced5e361414f
Jerome Kieffer <jerome.kieffer@esrf.fr>
2014-02-12 11:29:21 +0100

    plugin structure

---
+++ dahu/plugin.py
@@ -0,0 +1,46 @@
+import os
+
+class Plugin(object):
+    DEFAULT_SET_UP = "setup"      # name of the method used to set-up the plugin (close connection, files)
+    DEFAULT_PROCESS = "process"   # specify how to run the default processing
+    DEFAULT_TEAR_DOWN = "teardown"# name of the method used to tear-down the plugin (close connection, files)
+    def __init__(self):
+        """         
+        
+        """
+        self.init_param=None
+        self.output = None
+        self._logging = [] #
+
+    def setup(self, kargs=None):
+        """
+        This is the second constructor to setup 
+        input variables and possibly initialize
+        some objects 
+        """
+        self.init_param = kargs
+
+
+    def process(self, kargs=None):
+        """
+        main processing of the plugin
+        """
+        pass
+
+    def teardown(self):
+        """
+        method used to tear-down the plugin (close connection, files)
+        """
+        pass
+
+    def get_info(self):
+        """
+        """
+        return os.path.linesep.join(self._logging)
+
+if __name__ == "__main__":
+    #here I should explain how to run the plugin as stand alone:
+    p = Plugin()
+    p.setup()
+    p.process()
+    p.teardown()
