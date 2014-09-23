444df507ef3ed35a8b4b9ca917bc8830647ebdd6
kieffer <jerome.kieffer@esrf.fr>
2014-09-23 10:00:07 +0200

    debugging information

--- plugins/id02.py
+++ plugins/id02.py
@@ -269,6 +269,7 @@ input = {
         # Name
         HS32N = self.input.get("HS32N")
         if HS32N is not None:
+            print(HS32N)
             self.mcs_grp["HS32N"] = [str(i) for i in HS32N]
         # Mode
         HS32M = self.input.get("HS32M")
