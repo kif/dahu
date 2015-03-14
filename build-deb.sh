46d0d8375e4cefb6d9843e03e95da7bff57370b7
Jerome Kieffer <jerome.kieffer@esrf.fr>
2015-03-14 11:49:38 +0100

    update build for debian 8

--- build-deb.sh
+++ build-deb.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 rm -rf deb_dist
+export PYBUILD_DISABLE_python2=test
+export PYBUILD_DISABLE_python3=test
 python setup.py --command-packages=stdeb.command bdist_deb
 sudo dpkg -i deb_dist/python-*.deb
--- setup.py
+++ setup.py
@@ -10,16 +10,49 @@ __authors__ = ["Jérôme Kieffer"]
 __contact__ = "Jerome.Kieffer@ESRF.eu"
 __license__ = "MIT"
 __copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
-__date__ = "20140611"
+__date__ = "14/03/2015"
 __status__ = "development"
 
-import os, sys
-from distutils.core import setup, Extension, Command
+import os, sys, subprocess
+
+try:
+    # setuptools allows the creation of wheels
+    from setuptools import setup, Command
+    from setuptools.command.sdist import sdist
+    from setuptools.command.build_ext import build_ext
+    from setuptools.command.install_data import install_data
+except ImportError:
+    from distutils.core import setup, Command
+    from distutils.core import Extension
+    from distutils.command.sdist import sdist
+    from distutils.command.build_ext import build_ext
+    from distutils.command.install_data import install_data
+
 import glob
+
+cmdclass={}
 
 version = [eval(l.split("=")[1]) for l in open(os.path.join(os.path.dirname(
     os.path.abspath(__file__)), "dahu-src", "__init__.py"))
     if l.strip().startswith("version")][0]
+
+class PyTest(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        os.chdir("test")
+        errno = subprocess.call([sys.executable, 'test_all.py'])
+        if errno != 0:
+            raise SystemExit(errno)
+        else:
+            os.chdir("..")
+cmdclass['test'] = PyTest
 
 script_files = glob.glob("scripts/*")
 setup(name='dahu',
@@ -35,6 +68,6 @@ setup(name='dahu',
       packages=["dahu", "dahu.plugins"],
       package_dir={"dahu": "dahu-src", "dahu.plugins": "plugins" },
       test_suite="test",
-#      cmdclass=cmdclass,
+      cmdclass=cmdclass,
 #      data_files=data_files
       )
