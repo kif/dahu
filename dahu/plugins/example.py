b0d5c2c710c05fff924f332f641a4385bea0176f
Jerome Kieffer <jerome.kieffer@esrf.fr>
2016-11-04 11:39:40 +0100

    add a dummy plugin wich does nothing...

--- dahu/plugins/example.py
+++ dahu/plugins/example.py
@@ -14,11 +14,10 @@ __status__ = "development"
 version = "0.2.0"
 
 import time
-import os
-import numpy
 from dahu.plugin import plugin_from_function, Plugin, register
 import logging
 logger = logging.getLogger("plugin.example")
+
 
 @register
 class Cube(Plugin):
@@ -29,9 +28,15 @@ class Cube(Plugin):
         x = self.input.get("x", 0)
         self.output["result"] = x * x * x
 
+
 def square(x):
     return x * x
 plugin_from_function(square)
+
+
+def noop(x):
+    return x
+plugin_from_function(noop)
 
 
 @register
@@ -42,7 +47,7 @@ class Sleep(Plugin):
     def process(self):
         if self.input is None:
             logger.warning("input is None, defaulting to 10 sec")
-            t=10
+            t = 10
         else:
             t = self.input.get("t", 10)
         time.sleep(t)
