d624a3801a688d91ff08ea4fd4ef4b1152b8236c
Jerome Kieffer <jerome.kieffer@esrf.fr>
2020-02-11 13:49:20 +0100

    Factorize the class method

--- plugins/bm29/common.py
+++ plugins/bm29/common.py
@@ -11,13 +11,23 @@ __authors__ = ["Jérôme Kieffer"]
 __contact__ = "Jerome.Kieffer@ESRF.eu"
 __license__ = "MIT"
 __copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
-__date__ = "10/02/2020"
+__date__ = "11/02/2020"
 __status__ = "development"
 version = "0.0.1"
 
 from typing import NamedTuple
 import logging
-logger=logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
+
+
+def _fromdict(cls, dico):
+    "Mirror of _asdict: take the dict and populate the tuple to be returned"
+    try:
+        obj = cls(**dico)
+    except TypeError as err:
+        logger.warning("TypeError: %s", err)
+        obj = cls(**{key: dico[key] for key in [i for i in cls._fields if i in dico]})
+    return obj
 
 
 class Sample(NamedTuple):
@@ -39,16 +49,8 @@ class Sample(NamedTuple):
     storage_temp: float=None
     exposure_temp: float=None
 
-    @classmethod
-    def _fromdict(cls, dico):
-        "Mirror of _asdict: take the dict and populate the tuple to be returned"
-        try:
-            obj = cls(**dico)
-        except TypeError as err:
-            logger.warning("TypeError: %s", err)
-            intersection = [i for i in cls._fields if i in dico]
-            obj = cls(**{key: dico[key] for key in intersection})
-        return obj
+    _fromdict = classmethod(_fromdict)
+    
     
         
 class Ispyb(NamedTuple):        
@@ -57,13 +59,4 @@ class Ispyb(NamedTuple):
     passwd: str=None
     pyarch: str=None
 
-    @classmethod
-    def _fromdict(cls, dico):
-        "Mirror of _asdict: take the dict and populate the tuple to be returned"
-        try:
-            obj = cls(**dico)
-        except TypeError as err:
-            logger.warning("TypeError: %s", err)
-            intersection = [i for i in cls._fields if i in dico]
-            obj = cls(**{key: dico[key] for key in intersection})
-        return obj
+    _fromdict = classmethod(_fromdict)
