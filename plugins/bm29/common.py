83067a399345ab3441019a404f0d12b45107add9
Jerome Kieffer <jerome.kieffer@esrf.fr>
2020-02-12 09:53:34 +0100

    New function for finding the longest set of curves which are statistically equivalent

--- plugins/bm29/common.py
+++ plugins/bm29/common.py
@@ -11,7 +11,7 @@ __authors__ = ["Jérôme Kieffer"]
 __contact__ = "Jerome.Kieffer@ESRF.eu"
 __license__ = "MIT"
 __copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
-__date__ = "11/02/2020"
+__date__ = "12/02/2020"
 __status__ = "development"
 version = "0.0.1"
 
@@ -32,17 +32,17 @@ def _fromdict(cls, dico):
 
 class Sample(NamedTuple):
     """ This object represents the sample with the following representation 
-    {
-        "code": "bsa",
-        "comments": "protein name",
+      "sample": {
+        "name": "bsa",
+        "description": "protein description like Bovine Serum Albumin",
         "buffer": "description of buffer, pH, ...",
         "concentration": 0,
         "hplc": "column name and chromatography conditions",
-        "storage_temp": 20,
-        "exposure_temp": 20},
+        "temperature": 20,
+        "temperature_env": 20},  
     """
-    code: str=None
-    comments: str=None
+    name: str=None
+    description: str=None
     buffer: str=None
     concentration: float=None
     hplc: str=None
@@ -60,3 +60,36 @@ class Ispyb(NamedTuple):
     pyarch: str=None
 
     _fromdict = classmethod(_fromdict)
+
+
+class EquivalentFrames(NamedTuple):
+    start: int=0
+    end: int=-1
+
+def get_equivalent_frames(proba, absolute=0.1, relative=0.2):
+    """This function return the start and end index of a set of equivalent data:
+    Note the end-index is excluded...
+    
+    :param proba: 2D array with the probablility that 2 dataset are array_equivalent
+    :param absolute: minimum probablity of 2 dataset to be considered equivalent
+    :param absolute: minimum probablity of 2 adjacents dataset to be considered equivalent
+    """
+    res = []
+    sizes = []
+    size = len(proba)
+    diag = numpy.diagonal(proba, 1) > relative
+    "diag is the true if this a start point worth to be considered"
+    for start, valid in enumerate(diag):
+        if not valid:
+            continue
+        # searching for the end-point which is the first invalid
+        invalid = proba[start, :] < absolute
+        invalid[:start] = False  # only search for greater indices
+        wend = numpy.where(invalid)[0]
+        end = wend[0] if len(wend) else size
+        sizes.append(end-start)
+        res.append(EquivalentFrames(start, end))
+    return res[numpy.argmax(sizes)]
+
+        
+            
