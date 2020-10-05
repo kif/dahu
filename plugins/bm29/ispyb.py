0e83e83fb21c8fc01455b93f42dee5b2ba68187e
Jerome Kieffer <jerome.kieffer@terre-adelie.org>
2020-10-05 09:16:14 +0200

    comment out unfinished code to make pyflakes hapy

--- plugins/bm29/ispyb.py
+++ plugins/bm29/ispyb.py
@@ -19,13 +19,15 @@ import logging
 logger = logging.getLogger("bm29.ispyb")
 import os
 import shutil
-import numpy    
+import numpy
 from suds.client            import Client
 from suds.transport.https   import HttpAuthenticated
 from freesas.collections import RG_RESULT
 
+
 class IspybConnector:
     "This class is a conector to the web-service"
+
     def __init__(self, url, login=None, passwd=None, pyarch=None, collection_id=-1, measurement_id=-1):
         """Constructor of the ISPyB connections
         
@@ -42,10 +44,10 @@ class IspybConnector:
             self.pyarch = os.path.abspath(pyarch)
         else:
             logger.error("No `pyarch` destination provided ... things will go wrong")
-            
+
         self.collection_id = collection_id
         self.measurement_id = measurement_id
-        
+
     def send_averaged(self, data):
         """Send this to ISPyB and backup to PyArch
         
@@ -58,12 +60,12 @@ class IspybConnector:
         basename = data.pop("basename")
         discarded = []
         frames = []
-        merged = data.pop("merged") 
-        
+        merged = data.pop("merged")
+
         aver_data = data.pop("avg")
         averaged = self.save_curve("avg", aver_data, basename)
-            
-        for k,v in data.items():
+
+        for k, v in data.items():
             if isinstance(k, int):
                 fn = self.save_curve(k, v, basename)
                 if k in merged:
@@ -75,7 +77,7 @@ class IspybConnector:
                                         str(frames),
                                         str(discarded),
                                         str(averaged))
-    
+
     def save_curve(self, index, integrate_result, basename="frame"):
         """Save a  1D curve into the pyarch. Not those file do not exist outside pyarch
         
@@ -87,24 +89,24 @@ class IspybConnector:
         if not os.path.isdir(dest):
             os.makedirs(dest)
         if isinstance(index, int):
-            filename = os.path.join(dest, "%s_%04d.dat"%(basename,index))
+            filename = os.path.join(dest, "%s_%04d.dat" % (basename, index))
         else:
-            filename = os.path.join(dest, "%s_%s.dat"%(basename, index))
+            filename = os.path.join(dest, "%s_%s.dat" % (basename, index))
         sasl = numpy.vstack((integrate_result.radial, integrate_result.intensity, integrate_result.sigma))
         numpy.savetxt(filename, sasl.T)
         return filename
-        
+
     def send_subtracted(self, data):
         """send the result of the subtraction to Ispyb
         
         :param data: a dict with all information to be saved in Ispyb
-        """ 
-        guinier = data.get("guinier", *([-1.]*8))
-        gnom = data.get("bift", *([-1.]*8))
+        """
+        guinier = data.get("guinier", *([-1.] * 8))
+        gnom = data.get("bift", *([-1.] * 8))
         subtracted = data.get("subtracted")
         sub = self.save_curve("subtracted", subtracted, data.get("basename", "frame"))
         sasm = numpy.vstack((subtracted.radius, subtracted.intensity, subtracted.sigma))
-        
+        """ TODO
         self.client.service.addSubtraction(str(self.measurement_id),
                                            str(guinier.Rg),
                                            str(guinier.sigma_Rg),
@@ -128,3 +130,4 @@ class IspybConnector:
                                            self.guinierPlot,                       #guinierPlotFilePath,
                                            self.kratkyPlot,                        #kratkyPlotFilePath,
                                            self.gnomFile)                          #gnomOutputFilePath
+"""
