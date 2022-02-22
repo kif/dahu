521af63facea88e203b3733c8f12a8007e165655
Jerome Kieffer <jerome.kieffer@esrf.fr>
2022-02-22 13:06:33 +0100

    minor rework

--- plugins/id27.py
+++ plugins/id27.py
@@ -1,14 +1,20 @@
+import os
+import shutil
+import collections
+import io
 import logging
 from dahu.plugin import Plugin
 from dahu.factory import register
+import fabio
+logger = logging.getLogger("id27")
 
-logger = logging.getLogger("id15")
-
-import os, re, glob, sys, shutil, collections, io
 from cryio import crysalis
 import subprocess
-import threading
-import multiprocessing
+# import threading
+# import multiprocessing
+
+WORKDIR = "/scratch/shared"
+
 
 def crysalis_config(calibration_path,calibration_name, number_of_frames,  omega_start, omega_step, center, distance, wave_length, exposure_time):
     crysalis_files = {
@@ -75,7 +81,7 @@ def createCrysalis(scans, crysalis_dir, basename):
     crysalis.saveCrysalisExpSettings(crysalis_dir)
 
 
-def create_par_file(crysalis_files,crysalis_dir, basename):
+def create_par_file(crysalis_files, crysalis_dir, basename):
 
     new_par = os.path.join(crysalis_dir,basename+'.par')
 
@@ -88,57 +94,96 @@ def create_par_file(crysalis_files,crysalis_dir, basename):
                     new_file.write(line)
 
 
+def create_rsync_file(filename):
+    """create 2 files: 
+        * .source with the LIMA-HDF5 file 
+        * a bash script to synchronise back the reduced crysalis dataset
+    
+    :param filename: name of the LIMA-HDF5 file with the images
+    :return: the path of the crysalis directory  
+    """
+    dest_dir = "/".join(filename.split("/")[3:-1]) #strip /data/visitor ... filename.h5
+    dest_dir = os.path.join(WORKDIR, dest_dir)
+    crysalis_dir = os.path.join(dest_dir, 'esp')
+    if not os.path.exists(crysalis_dir):
+            os.makedirs(crysalis_dir)
+    with open(os.path.join(dest_dir, ".source"), "w") as source:
+        source.write(filename)
+    with open(os.path.join(dest_dir, "sync"), "w") as source:
+        source.write(f'#!/bin/sh\nrsync -avx {os.path.join(dest_dir, "esp")} {os.path.dirname(filename)}\n')
+    return crysalis_dir
 
-########################################################## Start the server part
 
-def crysalis_conversion(wave_length=None,distance=None,
-                            center=(None,None),
-                            omega_start=None,omega_step=None,
-                            exposure_time=None,
-                            number_of_points=None,
-                            file_source_path=None,
-                            scan_name=None,
-                            calibration_path="",calibration_name="", **kwargs):
-        print('start data convertion with parameters:\n',
-              f'wave_length={wave_length}\n'
-              f'distance={distance}\n',
-              f'center={center}\n',
-              f'omega_start={omega_start}\n',
-              f'omega_step={omega_step}\n',
-              f'file_source_path={file_source_path}\n',
-              f'scan_name={scan_name}')
+def crysalis_conversion(wave_length=None, distance=None,
+                        center=(None, None),
+                        omega_start=None,omega_step=None,
+                        exposure_time=None,
+                        number_of_points=None,
+                        file_source_path=None,
+                        scan_name=None,
+                        calibration_path="", calibration_name="",
+                        **kwargs):
+        """Run the `eiger2crysalis` script synchronously
+         
+        """
+        logger.info('start data convertion with parameters:\n'+
+                    f'wave_length={wave_length}\n'+
+                    f'distance={distance}\n'+
+                    f'center={center}\n'+
+                    f'omega_start={omega_start}\n'+
+                    f'omega_step={omega_step}\n'+
+                    f'file_source_path={file_source_path}\n'+
+                    f'scan_name={scan_name}')
 
         script_name = 'eiger2crysalis'
 
-        dirname = os.path.split(file_source_path)
+        crysalis_dir = create_rsync_file(file_source_path)
+        
         parameters = [script_name,
                       "-w", f'{wave_length}',
                       "-d", f'{distance}',
                       "-b", f'{center[0]}', f'{center[1]}',
                       f"--omega={omega_start}+{omega_step}*index",
                       file_source_path,
-                      "-o", os.path.join(dirname[0],'esp',f'{scan_name}_1_''{index}.esperanto')]
-        print('starts with parameters:',parameters)
+                      "-o", os.path.join(crysalis_dir, f'{scan_name}_1_''{index}.esperanto')]
+        logger.info('starts with parameters: %s',parameters)
 
         crysalis_files, scans = crysalis_config(calibration_path, calibration_name, number_of_points,  omega_start, omega_step, center, distance, wave_length, exposure_time)
-        crysalis_dir = os.path.join(dirname[0], 'esp')
-        isExist = os.path.exists( crysalis_dir)
-        if not isExist:
-            os.makedirs(crysalis_dir)
+
         copy_set_ccd(crysalis_files,crysalis_dir, scan_name )
         createCrysalis(scans, crysalis_dir, scan_name)
         create_par_file(crysalis_files,crysalis_dir, scan_name)
 
         return subprocess.run(parameters)
 
+##########################
+# Start the server part
+##########################
 
 @register
 class CrysalisConversion(Plugin):
+    """
+    This is the basic plugin of cysalis conversion
+       
+    Typical JSON file:
+    {"wave_length": 1.54,
+     "distance": 100,
+     "center": [400,500],
+     "omega_start":-90,
+     "omega_step": 1,
+     "exposure_time":0.1,
+     "number_of_points":180,
+     "file_source_path": "/data/id27/inhouse/sample/example.h5",
+     "scan_name": "scan",
+     "calibration_path":"/data/id27/inhouse/sample/vanadinite",
+     "calibration_name":"vanadinite"
+    }
+    
+    """
+    
     def process(self):
         Plugin.process(self)
         if self.input is None:
             logger.warning("input is None")
         result = crysalis_conversion(**self.input)
-        self.log_warning(str(result))
-        
-                
+        self.output["results"] = result        
