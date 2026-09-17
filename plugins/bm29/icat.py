0684ca3fe9696462781b1c9295f881cf5d7cba96
Jerome Kieffer <jerome.kieffer@esrf.fr>
2026-09-17 14:28:50 +0200

    close #106

--- plugins/bm29/icat.py
+++ plugins/bm29/icat.py
@@ -10,7 +10,7 @@ __authors__ = ["Jérôme Kieffer"]
 __contact__ = "Jerome.Kieffer@ESRF.eu"
 __license__ = "MIT"
 __copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
-__date__ = "16/09/2026"
+__date__ = "17/09/2026"
 __status__ = "development"
 __version__ = "0.3.0"
 
@@ -25,6 +25,11 @@ try:
 except ImportError:
     logger.error("iCat connection will no work")
     IcatClient = None
+try:
+    from esrf_ontologies import technique
+except:
+    technique = None
+    METADATA = {'technique_pid': 'https://w3id.org/PaN/ESRFET#BioSAXS https://w3id.org/PaN/ESRFET#SAXS', 'definition': 'BioSAXS SAXS', 'technique_pid_esrfet_version': '0.1.0'}
 
 version = __version__
 
@@ -97,6 +102,11 @@ def send_icat(proposal=None, beamline=None, sample=None, dataset=None, path=None
 
     if metadata is None:
         metadata = {}
+    if technique is not None:
+        metadata.update(technique.get_technique_metadata("SAXS", "BIOSAXS").get_dataset_metadata())
+    else:
+        metadata.update(METADATA)
+
     metadata["definition"] = "SAXS"
    # metadata["Sample_name"] = sample
 
